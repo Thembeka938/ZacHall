d9d9466c559b5ed5817f0814c2aa0e20dd405915
Thembeka Nguza <Thembeka.Nguza@ekurhuleni.gov.za>
2023-07-17 21:46:59 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { RightbarComponent } from './rightbar/rightbar.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { TopbarComponent } from './topbar/topbar.component';
+import { TopRightComponent } from './top-right/top-right.component';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    RightbarComponent,
+    TopbarComponent,
+    TopRightComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatDividerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/top-right/top-right.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-right',
+  templateUrl: './top-right.component.html',
+  styleUrls: ['./top-right.component.scss']
+})
+export class TopRightComponent {
+
+}
